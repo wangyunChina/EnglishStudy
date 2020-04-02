f5da211e56e80a9793d43412c53cb141fa37be5d
unknown <unknown@example.com>
2020-04-02 13:58:40 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,153 @@
+//app.js
+App({
+  onLaunch: function () {
+    const _this = this;
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+          //未授权
+          wx.showModal({
+            title: '请求授权当前位置',
+            content: '需要获取您的地理位置，请确认授权',
+            success: function (res) {
+              if (res.cancel) {
+                //取消授权
+                wx.showToast({
+                  title: '拒绝授权',
+                  icon: 'none',
+                  duration: 1000
+                })
+              } else if (res.confirm) {
+                //确定授权，通过wx.openSetting发起授权请求
+                wx.openSetting({
+                  success: function (res) {
+                    if (res.authSetting["scope.userLocation"] == true) {
+                      wx.showToast({
+                        title: '授权成功',
+                        icon: 'success',
+                        duration: 1000
+                      })
+                      //再次授权，调用wx.getLocation的API
+                      _this.goAddress();
+                    } else {
+                      wx.showToast({
+                        title: '授权失败',
+                        icon: 'none',
+                        duration: 1000
+                      })
+                    }
+                  }
+                })
+              }
+            }
+          })
+        } else if (res.authSetting['scope.userLocation'] == undefined) {
+          //用户首次进入页面,调用wx.getLocation的API
+          _this.goAddress();
+        }
+        else {
+          // console.log('授权成功')
+          //调用wx.getLocation的API
+          _this.goAddress();
+        }
+
+
+
+      }
+      
+    })
+    
+  },
+  goAddress:function(){
+    const _this = this;
+    wx.getSetting({
+      success: (res) => {
+        // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
+        // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
+        // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+          //未授权
+          wx.showModal({
+            title: '请求授权当前位置',
+            content: '需要获取您的地理位置，请确认授权',
+            success: function (res) {
+              if (res.cancel) {
+                //取消授权
+                wx.showToast({
+                  title: '拒绝授权',
+                  icon: 'none',
+                  duration: 1000
+                })
+              } else if (res.confirm) {
+                //确定授权，通过wx.openSetting发起授权请求
+                wx.openSetting({
+                  success: function (res) {
+                    if (res.authSetting["scope.userLocation"] == true) {
+                      wx.showToast({
+                        title: '授权成功',
+                        icon: 'success',
+                        duration: 1000
+                      })
+                      //再次授权，调用wx.getLocation的API
+                      _this.goAddress();
+                    } else {
+                      wx.showToast({
+                        title: '授权失败',
+                        icon: 'none',
+                        duration: 1000
+                      })
+                    }
+                  }
+                })
+              }
+            }
+          })
+        } else if (res.authSetting['scope.userLocation'] == undefined) {
+          //用户首次进入页面,调用wx.getLocation的API
+          _this.goAddress();
+        }
+        else {
+          // console.log('授权成功')
+          //调用wx.getLocation的API
+          _this.goAddress();
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+  },
+  data:{
+    userLocation: null,
+  }
+  
+
+})
---
+++ pages/index/index.js
@@ -0,0 +1,98 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var QQMapWX = require('../../lib/qqmap-wx-jssdk.js');
+var qqmapsdk;
+Page({
+  data: {
+   temp:"",
+    temperature:"",
+    weather: "",
+    city: "",
+    week: "",
+    wind: "",
+    select:false,
+    cid:null,
+  },
+  getWeather:function(cid){
+    var that=this;
+    wx.request({
+      url: 'https://sapi.k780.com/?app=weather.today&weaid='+cid+'&appkey=49970&sign=6211777879fabf24dbaa4a04117971be&format=json ',
+      method:"GET",
+      header: {
+        'Content-Type': 'json'
+      },
+      success:function(res)
+      {
+        var data=res.data.result;
+        console.log(data);
+        that.setData({ temp:data.temp_curr });
+        that.setData({temperature:data.temperature});
+        that.setData({ weather: data.weather});
+        that.setData({city:data.citynm});
+        that.setData({ week: data.week});
+        that.setData({ wind: data.wind});
+      }
+    })
+  },
+  
+  onLoad:function(){
+    var that=this;
+    qqmapsdk = new QQMapWX({
+
+      key: 'RPIBZ-X7IL4-BTUUB-DY3OS-S2SFF-TCFS4'
+
+    });
+    wx.getLocation({
+      success: function(res) {
+        console.log(res);
+        var longitude = res.longitude;
+        var latitude = res.latitude;
+       
+          let qqmapsdk = new QQMapWX({
+            key: 'PKIBZ-PRXC4-ZRHUF-XQ6XH-AW2K7-F7FUM'
+          })
+          qqmapsdk.reverseGeocoder({
+            location: {
+              latitude: latitude,
+              longitude: longitude
+            },
+            success: function (res) {
+              var city = res.result.address_component.district;
+              console.log(city);
+              var reg=new RegExp('区','gi');
+              city=city.replace(reg,'');
+              var reg = new RegExp('市', 'gi');
+              city = city.replace(reg, '');
+              var reg = new RegExp('自治区', 'gi');
+              city = city.replace(reg, '');
+              var reg = new RegExp('自治县', 'gi');
+              city = city.replace(reg, '');
+              console.log(city);
+              that.setData({ cid: city})
+              that.getWeather(city);
+              console.log(res, 'success')
+
+            },
+            fail: function (res) {
+              console.log(res, 'fail')
+            }
+          })
+        
+      
+       
+
+      },
+    })
+
+
+
+  },
+  clikme:function(){
+    console.log("clik me");
+  },
+  
+
+
+  
+})
